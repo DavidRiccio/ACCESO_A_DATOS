c23f956e8c506885ce21b5ce2debe7b84153d9ec
DavidRiccio <dricciodeabreu1@gmail.com>
2025-09-26 16:13:54 +0100

    Tarea 3: La venganza de los Sith

--- PHP/tarea_3/ejercicios/ejercicio12.php
+++ PHP/tarea_3/ejercicios/ejercicio12.php
@@ -3,8 +3,12 @@
 $ruta = "ficheros/ranking.txt";
 $lineas = file($ruta);
 $resultado = [];
-foreach ($lineas as $l) {
-   print_r( explode(": ",$l));
-}
 
+
+foreach ($lineas as $l) {
+    $helper = explode(": ",$l) ;
+    $resultado[$helper[0]] = $helper[1];
+    arsort($resultado);
+}
+print_r($resultado);
 ?>
