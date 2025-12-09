3e11c0d547dfce8c1a3b47fafd8bd78562de1870
DavidRiccio <dricciodeabreu1@gmail.com>
2025-12-09 20:26:14 +0000

    Se hace la practica 02 de conexion de una api y se empieza la 03

--- JAVA/spring-rest-producto/spring-rest-producto/src/main/java/com/docencia/rest/repository/interfaces/DetalleProductoRepository.java
+++ JAVA/spring-rest-producto/spring-rest-producto/src/main/java/com/docencia/rest/repository/interfaces/DetalleProductoRepository.java
@@ -3,10 +3,11 @@ package com.docencia.rest.repository.interfaces;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.docencia.rest.modelo.DetalleProductoDocument;
 
-
+@Repository
 public interface DetalleProductoRepository extends MongoRepository<DetalleProductoDocument, Integer> {
     /**
      * Busca un producto a traves de su id
