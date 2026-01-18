81092d768adbaa69d0778c5fc749ad8518ce66ab
DavidRiccio <dricciodeabreu1@gmail.com>
2026-01-18 11:39:11 +0000

    Se termina la practica 2, conectamos el form con el back y ponemos estilos

--- ANGULAR/practica-01/src/app/pages/task-new/task-new.component.ts
+++ ANGULAR/practica-01/src/app/pages/task-new/task-new.component.ts
@@ -1,8 +1,7 @@
-import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { TasksService } from '../../services/tasks.service';
-import { NewTask } from '../../models/task.model';
+import { TasksApiService } from '../../services/tasks-api.service'; 
 
 @Component({
   selector: 'app-task-new',
@@ -13,37 +12,36 @@ import { NewTask } from '../../models/task.model';
 })
 export class TaskNewComponent {
   private fb = inject(FormBuilder);
-  private tasks = inject(TasksService);
+  private tasksApi = inject(TasksApiService); 
   private router = inject(Router);
 
+  
   form = this.fb.nonNullable.group({
-    title: this.fb.nonNullable.control('', [Validators.required, Validators.minLength(3)]),
-    description: this.fb.nonNullable.control(''),
-    completada: this.fb.nonNullable.control(false),
+    title: ['', [Validators.required, Validators.minLength(3)]],
+    description: [''],
+    completada: [false],
   });
 
-  @Output() crear = new EventEmitter<NewTask>();
-
-  title: string = '';
-  description: string = '';
-  error: string | null = null;
-
   enviar(): void {
-    const t = this.title.trim();
-    if (!t) {
-      this.error = 'El título es obligatorio';
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
       return;
     }
-    this.error = null;
 
-    this.crear.emit({
-      title: t,
-      description: this.description.trim() || undefined,
-      completada: false,
+    
+    const newTask = this.form.getRawValue();
+
+    
+    this.tasksApi.create(newTask).subscribe({
+      next: () => {
+        console.log('Tarea creada con éxito');
+        this.router.navigateByUrl('/tareas'); 
+      },
+      error: (err) => {
+        console.error('Error al crear la tarea', err);
+        
+      }
     });
-
-    this.title = '';
-    this.description = '';
   }
 
   cancel() {
